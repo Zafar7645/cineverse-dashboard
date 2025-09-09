8a48bc91c834df9d8dbe80d01c6a641139d696d8
Zafar7645 <zafarshaikh645@gmail.com>
2025-09-09 21:11:34 +0530

    add router test in not found component

--- src/app/not-found/not-found.component.spec.ts
+++ src/app/not-found/not-found.component.spec.ts
@@ -1,13 +1,17 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Router } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { NotFoundComponent } from './not-found.component';
 
 describe('NotFoundComponent', () => {
   let component: NotFoundComponent;
   let fixture: ComponentFixture<NotFoundComponent>;
+  let router: Router;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
+      imports: [RouterTestingModule],
       declarations: [NotFoundComponent]
     });
     fixture = TestBed.createComponent(NotFoundComponent);
@@ -17,5 +21,12 @@ describe('NotFoundComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should navigate to home on goHome()', () => {
+    const router = TestBed.inject(Router);
+    const navigateSpy = spyOn(router, 'navigateByUrl');
+    component.goHome();
+    expect(navigateSpy).toHaveBeenCalledWith('');
   });
 });
