769e1377d508117bd2d95f3fd164a43e2fffb87f
Zafar7645 <zafarshaikh645@gmail.com>
2025-09-09 21:13:32 +0530

    refactor app component and spec

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -19,11 +19,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app.title).toEqual('cineverse-dashboard');
   });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('cineverse-dashboard app is running!');
-  });
 });
