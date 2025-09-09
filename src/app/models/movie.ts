eb4626bc5abe95658af417cfd031c86ed4ecce0c
Zafar7645 <zafarshaikh645@gmail.com>
2025-09-10 00:34:27 +0530

    add movie details page

--- src/app/models/movie.ts
+++ src/app/models/movie.ts
@@ -1,3 +1,7 @@
+import { IGenre } from './genre';
+import { IProductionCompany, IProductionCountry } from './production-company';
+import { ISpokenLanguage } from './spoken-language';
+
 export interface IMovie {
   adult: boolean;
   backdrop_path: string;
@@ -11,6 +15,35 @@ export interface IMovie {
   release_date: string;
   title: string;
   video: boolean;
+  vote_average: number;
+  vote_count: number;
+}
+
+export interface IMovieDetails {
+  adult: boolean;
+  backdrop_path: string;
+  belongs_to_collection: string;
+  budget: number;
+  genres: IGenre[];
+  homepage: string;
+  id: number;
+  imdb_id: string;
+  origin_country: string[];
+  original_language: string;
+  original_title: string;
+  overview: string;
+  popularity: number;
+  poster_path: string;
+  production_companies: IProductionCompany[];
+  production_countries: IProductionCountry[];
+  release_date: string;
+  revenue: number;
+  runtime: number;
+  spoken_languages: ISpokenLanguage[];
+  status: string;
+  tagline: string;
+  title: string;
+  video: false;
   vote_average: number;
   vote_count: number;
 }
