91cc7db26253f861fcd847aca3ceb5468b24eb29
Zafar7645 <zafarshaikh645@gmail.com>
2025-09-10 17:11:05 +0530

    add search feature

--- src/app/movie-list/movie-list.component.ts
+++ src/app/movie-list/movie-list.component.ts
@@ -1,7 +1,15 @@
+import {
+  debounceTime,
+  distinctUntilChanged,
+  of,
+  Subject,
+  Subscription,
+  switchMap,
+} from 'rxjs';
+
 import { Component, OnInit } from '@angular/core';
 
 import { IMovie } from '../models/movie';
-import { IMovieList } from '../models/movie-list';
 import { MovieDbService } from '../services/movie-db.service';
 
 @Component({
@@ -10,9 +18,13 @@ import { MovieDbService } from '../services/movie-db.service';
   styleUrls: ['./movie-list.component.css'],
 })
 export class MovieListComponent implements OnInit {
-  movieList: IMovie[] = [];
+  popularMoviesList: IMovie[] = [];
+  displayedMovieList: IMovie[] = [];
   isLoading: boolean = false;
   error: string | null = null;
+
+  private searchQuery = new Subject<string>();
+  private searchSubscription!: Subscription;
 
   constructor(private movieDbService: MovieDbService) {}
 
@@ -22,7 +34,8 @@ export class MovieListComponent implements OnInit {
 
     this.movieDbService.getPopularMovies().subscribe({
       next: (movies) => {
-        this.movieList = movies.results;
+        this.popularMoviesList = movies.results;
+        this.displayedMovieList = this.popularMoviesList;
         this.isLoading = false;
       },
       error: (err: Error) => {
@@ -30,16 +43,34 @@ export class MovieListComponent implements OnInit {
         this.isLoading = false;
       },
     });
+
+    this.searchSubscription = this.searchQuery
+      .pipe(
+        debounceTime(500),
+        distinctUntilChanged(),
+        switchMap((query) => {
+          if (!query) {
+            this.displayedMovieList = this.popularMoviesList;
+            return of(null);
+          }
+
+          this.isLoading = true;
+          return this.movieDbService.searchMovie(query);
+        })
+      )
+      .subscribe((response) => {
+        if (response) {
+          this.displayedMovieList = response.results;
+        }
+        this.isLoading = false;
+      });
   }
 
   handleSearch(query: string): void {
-    this.movieDbService.searchMovie(query).subscribe({
-      next: (searchResults: IMovieList) => {
-        this.movieList = searchResults.results;
-      },
-      error: (err: Error) => {
-        this.error = err.message;
-      },
-    });
+    this.searchQuery.next(query);
+  }
+
+  ngOnDestroy(): void {
+    this.searchSubscription.unsubscribe();
   }
 }
